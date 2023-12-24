580be8684e2cfebb788718192d1e8015c2e6c42d
Prateek Goyal <gprateek015@gmail.com>
2023-12-24 23:16:47 +0530

    updated apiUrl

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
-export const apiUrl = 'http://13.233.254.168';
+// export const apiUrl = 'http://13.233.254.168';
+export const apiUrl = 'https://lendwise-server.vercel.app';
 export const AUTH_TOKEN = 'lendwise_auth_token';
